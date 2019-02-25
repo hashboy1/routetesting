73ca0af54515fb5a63f18a52f28506d2022dfa32
username <email>
2019-02-25 14:16:39 +0800

    20190225

---
+++ route/src/main/java/com/concurrent/SemaphoreDemo.java
@@ -0,0 +1,78 @@
+package com.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreDemo {
+
+	
+	public static void main(String[] args) throws Exception {
+		
+		SemaphoreDemo sd=new SemaphoreDemo();
+		sd.execute();
+	
+	
+	}
+	
+	
+	public void execute()
+	{
+		 ExecutorService tpe=Executors.newFixedThreadPool(100);
+		  Semaphore se=new Semaphore(2);
+		  
+		  for (int i =0;i<=100;i++)
+		  {
+		      tpe.execute(new MyRunabled(se,i));
+		  }
+		
+	}
+
+	
+	class MyRunabled implements Runnable
+	{
+        
+		Semaphore sp;
+		int userID ;
+		
+		MyRunabled(Semaphore sp,int userID)
+		{
+			this.sp=sp;
+			this.userID=userID;
+			
+		}
+		
+		
+		@Override
+		public void run() {
+			
+			try {
+				sp.acquire();
+				System.out.println("User ID:" + userID + " begin");
+				Thread.sleep((long) (Math.random()*1000));			
+				System.out.println("User ID:" + userID + " end");
+				sp.release();
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
--- route/src/main/java/com/hashcode/human.java
+++ route/src/main/java/com/hashcode/human.java
@@ -1,8 +1,10 @@
 package com.hashcode;
 
+import java.util.HashMap;
+
 public class human {
 	
-	private int a=1;
+	private static int a=1;
 	
 	public int getA() {
 		return a;
@@ -23,7 +25,17 @@ public class human {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//HashMap hm=new HashMap();
+		
+		System.out.println("abcdef".hashCode());
+		System.out.println("abcdef".hashCode()&15);
+    
+		
+		human hm=new human();
+		hm.setA(12);
+		System.out.println(hm.getA());
+		System.out.println(human.a);
 	}
 
 }
